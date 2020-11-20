0f50a92e3b3f55fff25a24e7260b25d7809d51ef
Chris C <epost345@gmail.com>
2020-11-20 19:50:52 +0200

    Edit function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
 from . import util
 import markdown2
+import random
 
 
 def index(request):
@@ -18,7 +18,8 @@ def title(request, title):
             "error_message": "Your requested page was not found."
         })
     return render(request, "encyclopedia/wiki/title.html", {
-        "title": markdown2.markdown(find_title)
+        "title": title,
+        "content": markdown2.markdown(find_title)
     })
 
 
@@ -55,15 +56,14 @@ def new(request):
     return render(request, "encyclopedia/new.html")
 
 
-def edit():
-    return None
-
-
-def random():
-    return None
-
-
-'''# def random(request, random):
-# return render(request, "encyclopedia/random.html", {
-    # "random": util.get_entry(random)
-# })'''
+def edit(request):
+    title = request.GET.get('q')
+    find_title = util.get_entry(title)
+    if request.method == "POST":
+        content = request.POST.get('content')
+        util.save_entry(title, content)
+        return redirect(f"wiki/{title}")
+    return render(request, "encyclopedia/edit.html", {
+        "title": title,
+        "content": markdown2.markdown(find_title)
+    })
